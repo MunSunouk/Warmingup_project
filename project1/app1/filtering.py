07890afaa77bae5d27e770af0cb914e51909abca
june0348 <june0348@naver.com>
2020-09-24 21:59:42 +0900

    보경님이 필터링 한것 수정

--- project1/app1/filtering.py
+++ project1/app1/filtering.py
@@ -1,50 +1,66 @@
 import pandas as pd
+import numpy as np
 
-def have_filter(data, have_list):
-    data = data # 전처리 된 데이터셋
-    have_list = have_list
-    
-    idx_list=[]
-    for i in have_list:
-        idx_list.append(data[(data['재료명'].str.startswith(i))|(data['재료명'].str.endswith(i))])
-
-    idx = idx_list[0]
-
-    for i in range(1,len(idx_list)):
-        idx_ = idx_list[i]
-        idx = pd.concat([idx, idx_])
-
-    data = idx.reset_index()
-    data.drop('index',axis=1, inplace=True)
-    print(data.head(30))
-
-    return data
-
+# data : 재료 데이터, noeat_list : 안먹는 리스트
 def noeat_filter(data, noeat_list):
-    data = data # 전처리 된 데이터셋
+    noeat_data = data.copy() # 전처리 된 데이터셋
     noeat_list = noeat_list
     
     idx_list=[]
     for i in noeat_list:
-        idx_list.append(data[(data['재료명'].str.startswith(i))|(data['재료명'].str.endswith(i))])
+        idx_list.append(noeat_data[(noeat_data['재료명'].str.startswith(i))|(noeat_data['재료명'].str.endswith(i))])
 
-    idx = idx_list[0]
+    # print(idx_list)
 
-    for i in range(1,len(idx_list)):
-        idx_ = idx_list[i]
-        idx = pd.concat([idx, idx_])
+    idx = np.sort(pd.concat(idx_list)['레시피 코드'].unique().tolist())
+    # print(idx)
 
-    print(len(idx))
-    print(len(data))
+    # data_noteat = []
+    noeat_data = noeat_data[~noeat_data['레시피 코드'].isin(idx)]
 
-    idx_num = idx['재료명'].index
-    data.drop(idx_num, inplace=True)
-    print(len(data))
-    print(data.head(30))
+    return noeat_data
 
-    return data
+# 안먹는 재료 걸러넨 데이터와 먹는 재료
+def have_filter(data, have_list):
+    have_data = data.copy() # 전처리 된 데이터셋
+    have_list = have_list
+    
+    idx_list=[]
+    for i in have_list:
+        idx_list.append(have_data[(have_data['재료명'].str.startswith(i))|(have_data['재료명'].str.endswith(i))])
 
-data = pd.read_csv('../../data/레시피_김준민/레시피+재료정보_김준민.csv', encoding='cp949')
+    # 필터링 된 레시피 코드들
+    idx = np.sort(pd.concat(idx_list)['레시피 코드'].unique().tolist())
+    idx = idx.tolist()
+    # print(idx)
+
+
+    return idx
+
+def get_filtering_df(data_info, data_igd, data_process, idx):
+    # data_info : 기본정보 데이터
+    # data_igd : 재료정보 데이터
+    # data_process : 과정정보 데이터
+
+    have_info_list = []
+    have_igd_list = []
+    have_process_list = []
+
+    for i in idx:
+        have_info_list.append(data_info[data_info['레시피 코드']==i])
+        have_igd_list.append(data_igd[data_igd['레시피 코드']==i])
+        have_process_list.append(data_process[data_process['레시피 코드']==i])
+
+    have_info = pd.concat(have_info_list)
+    have_igd = pd.concat(have_igd_list)
+    have_process = pd.concat(have_process_list)
+
+    return have_info, have_igd, have_process
+
+
+
+
+data = pd.read_csv('../project1/레시피_재료정보.csv', encoding='cp949')
 
 ingre_str = '-쌀,-금,-새우,-버섯,-멸치,감자,고구마,안심,마늘,계란,콩'
 ingre_list = ingre_str.split(',')
@@ -58,7 +74,17 @@ for i in ingre_list:
     else:
         have_list.append(i)
 
-print(noeat_list)
-print(have_list)
-noeat_filter(data, noeat_list)
-have_filter(data,have_list)
+# print(noeat_list)
+# print(have_list)
+# print(noeat_filter(have_filter(data,have_list), noeat_list))
+
+# noeat_filter(data, noeat_list)
+idx = have_filter(noeat_filter(data, noeat_list), have_list)
+df_info = pd.read_csv('../project1/레시피_기본정보.csv', encoding='cp949')
+df_igd = pd.read_csv('../project1/레시피_재료정보.csv', encoding='cp949')
+df_process = pd.read_csv('../project1/레시피_과정정보.csv', encoding='cp949')
+
+a, b, c = get_filtering_df(df_info, df_igd, df_process, idx)
+print(a)
+print(b)
+print(c)
