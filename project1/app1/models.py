9454d6e7df98df6d8ebe88b5dc69628064556e7b
Ubuntu <ubuntu@ip-172-31-36-5.ap-northeast-2.compute.internal>
2020-09-24 09:38:31 +0000

    model2

--- project1/app1/models.py
+++ project1/app1/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 import pandas as pd
+from adaptor.model import CsvModel
 
-df = pd.read_csv('test_csv.txt', sep = ';')
 
 
 # 필터된 데이터 프레임 result
-class basic(models.Model):
+class basic(CsvModel):
     recipe_code = models.IntegerField() # 레시피이름
     introduce = models.CharField() # 간략소개
     quantile = models.CharField(max_length = 4) # 분량
@@ -14,17 +14,31 @@ class basic(models.Model):
     image = models.CharField()
     calorie = models.IntegerField()
     
-class raw(models.Model):
+    class Meta:
+         delimiter = ";"
+    
+class raw(CsvModel):
     recipe_code = models.IntegerField()
     raw_name = models.CharField()
     raw_quantile = models.CharField()
+    
+    class Meta:
+         delimiter = ";"
 
-class process(models.Model):
+class process(CsvModel):
     recipe_code = models.IntegerField()
     recipe_process_Char = models.CharField()
     recipe_process_num = models.IntegerField()
     recipe_process_image = models.CharField()
     
+    class Meta:
+        delimiter = ";"
 
+df0 = basic.import_data(data = open("레시피_기본정보.csv"))
+df1 = raw.import_data(data = open("레시피_재료정보.csv"))
+df2 = process.import_data(data = open("레시피_과정정보.csv"))
+    
+first_line = df0[0]
+first_line.recipe_code
     
 
